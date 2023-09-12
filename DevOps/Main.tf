0bd1c7f591ce697e7d7c73b8f47b7c2356bbffc7
John <hunhun010164@gmail.com>
2023-09-12 18:15:26 +1000

    update

--- DevOps/Main.tf
+++ DevOps/Main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "p3l1" {
-  bucket = "p3l1-bucket"
+  bucket = "my-tf-example-bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "p3l1" {
@@ -9,8 +9,20 @@ resource "aws_s3_bucket_ownership_controls" "p3l1" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "p3l1" {
+  bucket = aws_s3_bucket.p3l1.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_acl" "p3l1" {
-  depends_on = [aws_s3_bucket_ownership_controls.p3l1]
+  depends_on = [
+    aws_s3_bucket_ownership_controls.p3l1,
+    aws_s3_bucket_public_access_block.p3l1,
+  ]
 
   bucket = aws_s3_bucket.p3l1.id
   acl    = "public-read"

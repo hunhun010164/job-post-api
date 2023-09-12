441fcce6d738c20fa05f5a67c2a2b277f309f56f
John <hunhun010164@gmail.com>
2023-09-12 21:44:59 +1000

    update

--- DevOps/Main.tf
+++ DevOps/Main.tf
@@ -1,16 +1,16 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-example-bucket"
+resource "aws_s3_bucket" "yyqp3l1" {
+  bucket = "my-tf-yyqp3l1-bucket"
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_ownership_controls" "yyqp3l1" {
+  bucket = aws_s3_bucket.yyqp3l1.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_public_access_block" "yyqp3l1" {
+  bucket = aws_s3_bucket.yyqp3l1.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -18,12 +18,12 @@ resource "aws_s3_bucket_public_access_block" "example" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "example" {
+resource "aws_s3_bucket_acl" "yyqp3l1" {
   depends_on = [
-    aws_s3_bucket_ownership_controls.example,
-    aws_s3_bucket_public_access_block.example,
+    aws_s3_bucket_ownership_controls.yyqp3l1,
+    aws_s3_bucket_public_access_block.yyqp3l1,
   ]
 
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.yyqp3l1.id
   acl    = "public-read"
 }

14d5c4acd5482940e318d78cbe7ebe0e55316ed7
John <hunhun010164@gmail.com>
2023-09-12 20:53:22 +1000

    update

--- DevOps/Main.tf
+++ DevOps/Main.tf
@@ -20,6 +20,7 @@ resource "aws_s3_bucket_public_access_block" "p3l1test" {
 
 resource "aws_s3_bucket_acl" "p3l1test" {
   depends_on = [
+    aws_s3_bucket_ownership_controls.p3l1,
     aws_s3_bucket_ownership_controls.p3l1test,
     aws_s3_bucket_public_access_block.p3l1test,
   ]

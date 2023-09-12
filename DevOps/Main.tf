4e924f86896cab432ff3d8955f992a37c3e95a29
John <hunhun010164@gmail.com>
2023-09-12 20:47:52 +1000

    update

--- DevOps/Main.tf
+++ DevOps/Main.tf
@@ -1,34 +1,3 @@
-resource "aws_s3_bucket" "p3l1test" {
-  bucket = "p3l1test"
-}
-
-resource "aws_s3_bucket_ownership_controls" "p3l1test" {
-  bucket = aws_s3_bucket.p3l1test.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "p3l1test" {
-  bucket = aws_s3_bucket.p3l1test.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_acl" "p3l1test" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.p3l1test,
-    aws_s3_bucket_public_access_block.p3l1test,
-  ]
-
-  bucket = aws_s3_bucket.p3l1test.id
-  acl    = "public-read"
-}
-
-
 resource "aws_s3_bucket" "p3l1" {
   bucket = "p3l1"
 }

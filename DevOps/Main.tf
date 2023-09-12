ce0851091879a4e082c895e63cde1a980755227f
John <hunhun010164@gmail.com>
2023-09-12 20:53:57 +1000

    update

--- DevOps/Main.tf
+++ DevOps/Main.tf
@@ -1,16 +1,16 @@
-resource "aws_s3_bucket" "p3l1test" {
-  bucket = "p3l1test"
+resource "aws_s3_bucket" "p3l1" {
+  bucket = "p3l1"
 }
 
-resource "aws_s3_bucket_ownership_controls" "p3l1test" {
-  bucket = aws_s3_bucket.p3l1test.id
+resource "aws_s3_bucket_ownership_controls" "p3l1" {
+  bucket = aws_s3_bucket.p3l1.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "p3l1test" {
-  bucket = aws_s3_bucket.p3l1test.id
+resource "aws_s3_bucket_public_access_block" "p3l1" {
+  bucket = aws_s3_bucket.p3l1.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -18,13 +18,12 @@ resource "aws_s3_bucket_public_access_block" "p3l1test" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "p3l1test" {
+resource "aws_s3_bucket_acl" "p3l1" {
   depends_on = [
     aws_s3_bucket_ownership_controls.p3l1,
-    aws_s3_bucket_ownership_controls.p3l1test,
-    aws_s3_bucket_public_access_block.p3l1test,
+    aws_s3_bucket_public_access_block.p3l1,
   ]
 
-  bucket = aws_s3_bucket.p3l1test.id
+  bucket = aws_s3_bucket.p3l1.id
   acl    = "public-read"
 }

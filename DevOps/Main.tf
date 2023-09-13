06885a22e640b2e28b910d3540cdffea2d4654a8
John <hunhun010164@gmail.com>
2023-09-13 13:23:38 +1000

    update

--- DevOps/Main.tf
+++ DevOps/Main.tf
@@ -1,14 +1,31 @@
 resource "aws_s3_bucket" "yyq" {
   bucket = "p3l1"
+}
 
-  tags = {
-    Name = "My bucket"
+resource "aws_s3_bucket_ownership_controls" "yyq" {
+  bucket = aws_s3_bucket.yyq.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_acl" "yyq_acl" {
+resource "aws_s3_bucket_public_access_block" "yyq" {
   bucket = aws_s3_bucket.yyq.id
-  acl    = "private"
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "yyq" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.yyq,
+    aws_s3_bucket_public_access_block.yyq,
+  ]
+
+  bucket = aws_s3_bucket.yyq.id
+  acl    = "public-read"
 }
 
 resource "aws_cloudfront_origin_access_control" "example" {

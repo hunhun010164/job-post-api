f2b0ee0d95c6f5832a99de4e56ccae55cb594828
John <hunhun010164@gmail.com>
2023-09-25 20:59:24 +1000

    update

--- DevOps/Main.tf
+++ DevOps/Main.tf
@@ -1,76 +1,92 @@
 provider "aws" {
-  region = "us-east-1" # You can change this to your preferred region.
+  region = "us-east-1"
 }
 
-# S3 Bucket
-resource "aws_s3_bucket" "react_app" {
-  bucket = "p3l1-1"
+
+resource "aws_s3_bucket" "yyq" {
+  bucket = "p3l1-2"
 }
-resource "aws_s3_bucket_website_configuration" "react_app_website" {
-  bucket = aws_s3_bucket.react_app.id
 
-  index_document {
-    suffix = "landing.html"
-  }
-
-  error_document {
-    key = "error.html"
+resource "aws_s3_bucket_ownership_controls" "yyq" {
+  bucket = aws_s3_bucket.yyq.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "null_resource" "build" {
-  provisioner "local-exec" {
-    command = "yarn install && yarn run build"
-  }
+resource "aws_s3_bucket_public_access_block" "yyq" {
+  bucket = aws_s3_bucket.yyq.id
 
-  depends_on = [aws_s3_bucket.react_app]
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
-resource "null_resource" "deploy" {
-  provisioner "local-exec" {
-    command = "aws s3 sync out/ s3://${aws_s3_bucket.react_app.bucket}"
-  }
+resource "aws_s3_bucket_acl" "yyq" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.yyq,
+    aws_s3_bucket_public_access_block.yyq,
+  ]
 
-  depends_on = [null_resource.build]
+  bucket = aws_s3_bucket.yyq.id
+  acl    = "public-read"
 }
 
-# Fetch the existing ACM Certificate
-data "aws_acm_certificate" "existing_cert" {
-  domain   = "p3.siemens.global"
-  statuses = ["ISSUED"]
-}
 
-# Fetch details of existing hosted zone
-data "aws_route53_zone" "existing" {
+
+resource "aws_route53_zone" "example_zone" {
   name = "p3.siemens.global"
 }
 
-# Add cloudfront origin access identity
-resource "aws_cloudfront_origin_access_identity" "oai" {
-  comment = "OAI for weather app"
+resource "aws_route53_record" "example_record" {
+  zone_id = aws_route53_zone.example_zone.zone_id
+  name    = "@"
+  type    = "A"
+
+  ttl = "300"
+
+  records = ["1.1.1.1"]
 }
 
-# CloudFront
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name = aws_s3_bucket.react_app.bucket_regional_domain_name
-    origin_id   = "S3Origin"
 
-    s3_origin_config {
-      origin_access_identity = "origin-access-identity/cloudfront/${aws_cloudfront_origin_access_identity.oai.id}"
-    }
+
+resource "aws_cloudfront_origin_access_control" "example" {
+  name                              = "example"
+  description                       = "Example Policy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+locals {
+  s3_origin_id = "p3l1"
+}
+
+resource "aws_cloudfront_distribution" "yyq_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.yyq.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.example.id
+    origin_id                = local.s3_origin_id
   }
 
   enabled             = true
   is_ipv6_enabled     = true
-  default_root_object = "landing.html"
+  comment             = "Some comment"
+  default_root_object = "index.html"
 
-  aliases             = ["myapptest.oceanzou.click"]
+  logging_config {
+    include_cookies = false
+    bucket          = "p3l1.s3.amazonaws.com"
+    prefix          = "myprefix"
+  }
+
+  aliases = ["p3.siemens.global"]
 
   default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    allowed_methods  = ["GET", "HEAD", "OPTIONS","POST"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3Origin"
+    target_origin_id = local.s3_origin_id
 
     forwarded_values {
       query_string = false
@@ -80,59 +96,81 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       }
     }
 
-    viewer_protocol_policy = "redirect-to-https"
+    viewer_protocol_policy = "allow-all"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
   }
 
-  viewer_certificate {
-    acm_certificate_arn            = data.aws_acm_certificate.existing_cert.arn
-    ssl_support_method             = "sni-only"
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
   }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
 
   restrictions {
     geo_restriction {
       restriction_type = "none"
+      locations        = ["US", "CA", "GB", "DE"]
     }
   }
 
-  price_class = "PriceClass_100"
-}
+  tags = {
+    Environment = "production"
+  }
 
-# Route53 Record for CloudFront
-resource "aws_route53_record" "www" {
-  zone_id = data.aws_route53_zone.existing.zone_id
-  name    = "myapptest.oceanzou.click"
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
-    evaluate_target_health = false
+  viewer_certificate {
+    acm_certificate_arn = data.aws_acm_certificate.example.arn
   }
 }
 
-# Simple S3 bucket policy for CloudFront to access the bucket
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.react_app.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow",
-        Principal = {
-          AWS = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.oai.id}"
-        },
-        Action   = "s3:GetObject",
-        Resource = "${aws_s3_bucket.react_app.arn}/*"
-      }
-    ]
-  })
+resource "aws_acm_certificate" "example" {
+  domain_name       = "p3.siemens.global"
+  validation_method = "DNS"
 }
 
-# Output the CloudFront domain name to access the app
-output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.s3_distribution.domain_name
+data "aws_acm_certificate" "example" {
+  domain   = "p3.siemens.global"
+  statuses = ["ISSUED"]
 }
